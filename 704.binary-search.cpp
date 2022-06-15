d4969b7e64b32accdd8a81d078c3d1cf0d923175
Yitao <jstzzyt01@gmail.com>
2022-06-15 16:05:13 -0400

    update 704.binary-search.cpp

---
+++ 704.binary-search.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=704 lang=cpp
+ *
+ * [704] Binary Search
+ *
+ * https://leetcode.com/problems/binary-search/description/
+ *
+ * algorithms
+ * Easy (55.20%)
+ * Likes:    5194
+ * Dislikes: 121
+ * Total Accepted:    974.1K
+ * Total Submissions: 1.8M
+ * Testcase Example:  '[-1,0,3,5,9,12]\n9'
+ *
+ * Given an array of integers nums which is sorted in ascending order, and an
+ * integer target, write a function to search target in nums. If target exists,
+ * then return its index. Otherwise, return -1.
+ * 
+ * You must write an algorithm with O(log n) runtime complexity.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [-1,0,3,5,9,12], target = 9
+ * Output: 4
+ * Explanation: 9 exists in nums and its index is 4
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [-1,0,3,5,9,12], target = 2
+ * Output: -1
+ * Explanation: 2 does not exist in nums so return -1
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 10^4
+ * -10^4 < nums[i], target < 10^4
+ * All the integers in nums are unique.
+ * nums is sorted in ascending order.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        vector<int> :: iterator it2=nums.begin();
+        vector<int> :: iterator it=std :: lower_bound(nums.begin(),nums.end(),target);
+        if(it==nums.end() || *it!=target) return -1;
+            else return (it-it2) ;
+    }
+};
+// @lc code=end
